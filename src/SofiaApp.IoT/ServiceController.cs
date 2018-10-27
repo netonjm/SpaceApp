801d97ccbd92061ab40751b2adee98e728e194d5
José Medrano <jose.medrano@xamarin.com>
2018-10-28 01:26:44 +0200

    Moves service to a file

--- src/SofiaApp.IoT/ServiceController.cs
+++ src/SofiaApp.IoT/ServiceController.cs
@@ -2,49 +2,12 @@
 using System.Web.Http;
 using System.Net;
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
-using System.IO;
-using IoTSharp.Components;
 
 namespace SofiaApp.IoT
 {
-	public static class SpaceService
-	{
-		public static IoTPin GreenLed, RedLed;
-
-		static SpaceService () {
-			GreenLed = new IoTPin (Connectors.GPIO27);
-			GreenLed.SetDirection (IoTPinDirection.DirectionOutInitiallyLow);
-			RedLed = new IoTPin (Connectors.GPIO22);
-			RedLed.SetDirection (IoTPinDirection.DirectionOutInitiallyLow);
-		}
-
-		public static string Speak (string text)
-		{
-			var proc = new Process {
-				StartInfo = new ProcessStartInfo {
-					FileName = "espeak",
-					Arguments = $"-v es \"{text}\"",
-					UseShellExecute = false,
-					RedirectStandardOutput = true,
-					CreateNoWindow = true
-				}
-			};
-
-			proc.Start ();
-			return proc.StandardOutput.ReadToEnd ();
-		}
-	}
 
 	public class AlertServiceController : ApiController
 	{
-
-		public AlertServiceController () {
-
-		}
-
 		[Route ("sofia/event")]
 		public HttpResponseMessage GetActionEvent ()
 		{
---
+++ src/SofiaApp.IoT/SpaceService.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using IoTSharp.Components;
+
+namespace SofiaApp.IoT
+{
+	public static class SpaceService
+	{
+		public static IoTPin GreenLed, RedLed;
+
+		static SpaceService ()
+		{
+			GreenLed = new IoTPin (Connectors.GPIO27);
+			GreenLed.SetDirection (IoTPinDirection.DirectionOutInitiallyLow);
+			RedLed = new IoTPin (Connectors.GPIO22);
+			RedLed.SetDirection (IoTPinDirection.DirectionOutInitiallyLow);
+		}
+
+		public static string Speak (string text)
+		{
+			var proc = new Process {
+				StartInfo = new ProcessStartInfo {
+					FileName = "espeak",
+					Arguments = $"-v es \"{text}\"",
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					CreateNoWindow = true
+				}
+			};
+
+			proc.Start ();
+			return proc.StandardOutput.ReadToEnd ();
+		}
+	}
+}
